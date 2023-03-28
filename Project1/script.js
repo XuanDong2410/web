6a82f05f578f9c715a1165af2a21526b979be97c
XuanDong2410 <daoxuandong7012@gmail.com>
2023-03-28 23:04:15 +0700

    day 2

--- Project1/script.js
+++ Project1/script.js
@@ -4,12 +4,20 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
-let x = 0;
+playerImage.src = 'img/shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 3;
+let gameFrame = 0;
+const staggerFrame = 5;
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(x, 50, 100, 100);
-     ctx.drawImage(playerImage, 0, 0, 300, 300, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrame) % 6;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage,frameX,frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0,  spriteWidth, spriteHeight);
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
