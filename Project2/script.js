88dce42f29a1f4ef319aada3ceed472cd44f987f
XuanDong2410 <daoxuandong7012@gmail.com>
2023-03-31 23:28:03 +0700

    day 5

--- Project2/script.js
+++ Project2/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 15;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'img/layer-1.png';
@@ -15,16 +15,55 @@ backgroundLayer4.src = 'img/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'img/layer-5.png';
 
-let x = 0;
-let x2 = 2400;
-function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if(x < -2400) x = 2400 + x2 - gameSpeed;
-    else x -= gameSpeed;
-    if(x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
-    requestAnimationFrame(animate);
-};
-animate();
+window.addEventListener('load',function(){
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showGameSpeed');
+    showGameSpeed.innerHTML = gameSpeed;
+    slider.addEventListener('change', function(e){
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = e.target.value;
+    })
+    
+    class Layer {
+        constructor(image, speedModifier){
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * speedModifier;
+        }
+        update(){
+            this.speed = gameSpeed * this.speedModifier;
+            if(this.x <= -this.width){
+                this.x = 0;
+            }
+            this.x = this.x - this.speed;
+        }
+        draw(){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1);
+    
+    const gameObject = [layer1, layer2, layer3, layer4, layer5]
+    
+    function animate(){
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        gameObject.forEach(object => {
+            object.update();
+            object.draw();
+        })
+        requestAnimationFrame(animate);
+    };
+    animate();
+    
+});
+
